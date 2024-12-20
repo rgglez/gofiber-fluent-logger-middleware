77988befcb454e76e6916836ec4bece8075bd22f
Rodolfo Gonzalez <rodolfo.gonzalez+github@gmail.com>
2024-12-20 01:26:39 -0600

    Initial code

---
+++ fluentlogger/fluentlogger.go
@@ -0,0 +1,131 @@
+package fiberfluentdlogger
+
+/*
+Copyright 2024 Rodolfo González González
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import (
+	"time"
+
+	"runtime/debug"
+
+	"github.com/fluent/fluent-logger-golang/fluent"
+	fiber "github.com/gofiber/fiber/v2"
+	"github.com/ztrue/tracerr"
+)
+
+//*****************************************************************************
+
+type LoggerConfig struct {
+	Host string // the fluentd server address
+	Port int    // the fluentd server port
+	Tag  string // the tag to be used for the messages
+}
+
+//-----------------------------------------------------------------------------
+
+// Logger is a struct that holds the Fluentd logger instance and configuration
+type Logger struct {
+	client *fluent.Fluent
+	tag    string
+}
+
+//-----------------------------------------------------------------------------
+
+// New initializes a Fluentd logger and returns a middleware
+func New(config LoggerConfig) (*Logger, error) {
+	// Initialize Fluentd logger
+	fluentLogger, err := fluent.New(fluent.Config{
+		FluentHost: config.Host,
+		FluentPort: config.Port,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &Logger{
+		client: fluentLogger,
+		tag:    config.Tag,
+	}, nil
+}
+
+//-----------------------------------------------------------------------------
+
+// Logger logs each request to Fluentd
+func (l *Logger) Logger() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		start := time.Now()
+		err := c.Next() // Process the request
+		latency := time.Since(start)
+
+		// Log data to Fluentd
+		logData := map[string]interface{}{
+			"method":        c.Method(),
+			"path":          c.Path(),
+			"status":        c.Response().StatusCode(),
+			"latency_ms":    latency.Milliseconds(),
+			"client_ip":     c.IP(),
+			"user_agent":    c.Get("User-Agent"),
+			"response_size": len(c.Response().Body()),
+		}
+		if err != nil {
+			logData["error"] = tracerr.SprintSource(err)
+		}
+
+		// Send to Fluentd
+		if err := l.client.Post(l.tag, logData); err != nil {
+			tracerr.PrintSource(err)
+		}
+
+		return err
+	}
+}
+
+//-----------------------------------------------------------------------------
+
+// PanicLogger logs details on panic to Fluentd
+func (l *Logger) PanicLogger() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		err := c.Next() // Process the request
+
+		// Check if there was a panic (status code 500 indicates a server error)
+		if c.Response().StatusCode() == fiber.StatusInternalServerError {
+			// Log data to Fluentd
+			logData := map[string]interface{}{
+				"method":     c.Method(),
+				"path":       c.Path(),
+				"client_ip":  c.IP(),
+				"user_agent": c.Get("User-Agent"),
+			}
+
+			// Optionally, include the details of the err
+			if err != nil {
+				logData["error"] = tracerr.SprintSource(err)
+			}
+
+			// Optionally, include stack trace if err is a panic
+			if err != nil {
+				logData["stacktrace"] = string(debug.Stack())
+			}
+
+			// Send to Fluentd
+			if err := l.client.Post(l.tag+".panic", logData); err != nil {
+				tracerr.PrintSource(err)
+			}
+		}
+
+		return err
+	}
+}
---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/rgglez/gofiber-fluent-middleware
+
+go 1.23.2
+
+require (
+	github.com/fluent/fluent-logger-golang v1.9.0
+	github.com/gofiber/fiber/v2 v2.52.5
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/philhofer/fwd v1.1.2 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/tinylib/msgp v1.1.8 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	github.com/ztrue/tracerr v0.4.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+)
